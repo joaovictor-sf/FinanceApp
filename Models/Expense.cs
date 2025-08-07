57504d927683a7ecde1d71b058cd3144868067ac
joao <joaovictorsantanaf@gmail.com>
2025-08-07 11:15:51 -0300

    first commit

---
+++ Models/Expense.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceApp.Models {
+    public class Expense {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }// Using decimal for currency is a common practice to avoid floating-point precision issues. If size is a concern, you might consider using a smaller type like `float` or 'double', but it is generally not recommended for financial calculations.
+        public string Category { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
